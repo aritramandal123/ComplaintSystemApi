40523e33e0ee3948cedfd17459d25edd43416d7b
NoWhere <72866179+MortalFifefly@users.noreply.github.com>
2026-01-14 11:09:04 +0530

    Update loginRouter.js

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-
 const { login } = require('../controllers/loginController');
-const cred = JSON.parse(fs.readFileSync('data/cred.json', 'utf8'));
 
 router.post('/user', (req, res) => {
     login(req, res, 'user');
