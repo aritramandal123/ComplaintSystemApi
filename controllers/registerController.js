7edae405d5334c64612b8395d5c01524d0e1c88a
NoWhere <hangup806@gmail.com>
2026-01-15 16:44:44 +0530

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -46,7 +46,7 @@ const register = async (req, res) => {
     res.status(201).json({
         message: 'User registered successfully',
         token: token,
-        userId: newUserCred.email
+        userId: newUserCred._id
     });
 };
 
